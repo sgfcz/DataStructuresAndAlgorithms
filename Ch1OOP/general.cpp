fd82fe201cc40bddb46b937b3e7003bc589a92e4
sgfcz <973829105@qq.com>
2021-02-04 00:00:59 +0800

    add

---
+++ Ch1OOP/general.cpp
@@ -0,0 +1,13 @@
+template<class genType, int size = 50>
+class genclass {
+    genType storage[size];
+};
+
+int main() 
+{
+    genclass<int> intobjec1;
+    genclass<int, 100> intobjec2;
+    genclass<float, 123> floatobject1;
+
+    return 0;
+} 
---
+++ Ch1OOP/package.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+class C {
+public:
+    C(string s = " ", int i = 0, double d = 1) {
+        dataMember1 = s;
+        dataMember2 = i;
+        dataMember3 = d;
+    }
+
+    void memberFunction1() {
+        cout << dataMember1 << ' ' << dataMember2 << ' '
+            << dataMember3 << endl;
+    }
+
+    void memberFunction2(int i, string s = "unknown") {
+        dataMember2 = i;
+        cout << i << " received from " << s << endl;
+    }
+protected:
+    string dataMember1;
+    int dataMember2; 
+    double dataMember3;
+};
+
+int main() 
+{
+    C object1("object1", 100, 200), object2("object"), object3;
+    object1.memberFunction1();
+    return 0;
+} 
