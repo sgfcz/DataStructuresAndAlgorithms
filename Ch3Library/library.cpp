7522b413f715cd20897a3938d804a5dc0a74c301
sgfcz <973829105@qq.com>
2021-02-23 22:55:52 +0800

    up library

--- Ch3Library/library.cpp
+++ Ch3Library/library.cpp
@@ -9,25 +9,54 @@ class Person;
 
 class Book {
 public:
-		Book() {
-				patron = 0;
-		}
+    Book() {
+        patron = 0;
+    }
+    bool operator== (const Book& bk) const {
+        return strcmp(title, bk.title) == 0;
+    }
 private:
-		char* title;
-		Patron* patron;
-		ostream& printBook(ostream&) const;
-		friend ostream& operator<< (ostream out, const Book& bk) {
-				return bk.printBook(out);
-		}
-		friend class CheckedOutBook;
-		friend Patron;
+    char* title;
+    Patron* patron;
+    ostream& printBook(ostream&) const;
+    friend ostream& operator<< (ostream out, const Book& bk) { //ÖØÔØÔËËã·û<<
+        return bk.printBook(out);
+    }
+    friend class CheckedOutBook;
+    friend Patron;
+    friend void includeBook();
+    friend void checkOutBook();
+    friend void returnBook();
+};
 
+class Author {
+public:
+    Author() {
+    }
+    bool operator== (const Author& ar) const { 
+    }
+private:
+    char* name;
+    list<Book> books;
+    ostream& printAuthor(ostream&) const;
+    friend ostream& operator<< (ostream& out, const Author& ar) {
+        return ar.printAuthor(out);
+    }
+    friend void includeBook();
+    friend void checkOutBook();
+    friend void returnBook();
+    friend class CheckedOutBook;
+    friend Patron;
+};
+
+class CheckedOutBook {
+public:
 };
 
 class Patron {
 public:
-		Patron() {
-		}
+    Patron() {
+    }
 private:
 
 };
