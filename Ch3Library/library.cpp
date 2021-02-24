754f41c944e00d84945560d774f79509db0f0fa9
sgfcz <973829105@qq.com>
2021-02-24 23:55:31 +0800

    up library

--- Ch3Library/library.cpp
+++ Ch3Library/library.cpp
@@ -19,7 +19,7 @@ private:
     char* title;
     Patron* patron;
     ostream& printBook(ostream&) const;
-    friend ostream& operator<< (ostream out, const Book& bk) { //ÖØÔØÔËËã·û<<
+    friend ostream& operator<< (ostream out, const Book& bk) {
         return bk.printBook(out);
     }
     friend class CheckedOutBook;
@@ -33,7 +33,8 @@ class Author {
 public:
     Author() {
     }
-    bool operator== (const Author& ar) const { 
+    bool operator== (const Author& ar) const {
+        return strcmp(name, ar.name) == 0;
     }
 private:
     char* name;
@@ -51,6 +52,22 @@ private:
 
 class CheckedOutBook {
 public:
+    CheckedOutBook(list<Author>::iterator ar,
+        list<Book>::iterator bk) {
+        author = ar;
+        book = bk;
+    }
+
+    bool operator== (const CheckedOutBook& bk) const {
+        return strcmp(author->name, bk.author->name) == 0 &&
+            strcmp(book->title, bk.book->title) == 0;
+    }
+private:
+    list<Author>::iterator author;
+    list<Book>::iterator book;
+    friend void checkOutBook();
+    friend void returnBook();
+    friend Patron;
 };
 
 class Patron {
@@ -58,5 +75,39 @@ public:
     Patron() {
     }
 private:
-
+    char* name;
+    list<CheckedOutBook> books;
+    ostream& printPatron(ostream&) const;
+    friend ostream& operator<< (ostream& out, const Patron& pn) {
+        return pn.printPatron(out);
+    }
+    friend void checkOutBook();
+    friend void returnBook();
+    friend Book;
 };
+
+list<Author> catalog['Z' + 1];
+list<Patron> people['Z' + 1];
+
+ostream& Author::printAuthor(ostream& out) const {
+    out << name << endl;
+    list<Book>::const_iterator ref = books.begin();
+    for ( ; ref != books.end(); ref++)
+        out << *ref; // overloaded <<
+	return out;
+}
+
+ostream& Book::printBook(ostream& out) const {
+    out << " * " << title;
+    if (patron != 0)
+        out << " - checked out to" << patron->name; //overloaded<<
+    out << endl;
+    return out;
+}
+
+template<class T>
+ostream& operator<< (ostream& out, const list<T>& lst) {
+    for (list<T>::const_iterator ref = lst.begin(); ref != lst.end(); ref++)
+        out << *ref; //overloaded<<
+    return out;
+}
